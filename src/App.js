753c8d3c3617119f84137bf0138469b5182b195f
lee <vawav11@gmail.com>
2022-05-24 23:45:16 +0900

    React 기초 및 실습

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseItem from "./components/Expenseltem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
